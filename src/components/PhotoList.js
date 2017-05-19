b915b28cac3c9cf073ad16b20009c8ae52f9ac17
Dương Đỗ <dustin.do95@gmail.com>
2017-05-19 20:06:06 +0700

    Hide photo list scrollbar

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -39,36 +39,41 @@ export default class PhotoList extends React.Component {
         ref={element => this.container = element}
         style={{
           position: 'fixed',
-          top: 70, left: 10, bottom: 10,
-          display: 'flex', flexDirection: 'column',
+          top: 70, left: 0, bottom: 10, width: 40,
+          padding: 10,
           overflowY: 'auto', overflowX: 'hidden',
-          marginRight: -50, paddingRight: 50,
-          opacity: this.state.containerOpacity || 0.8,
-          transition: 'all .5s',
+          marginRight: -50,
+          paddingRight: 50,
           filter: `blur(${loading ? 2 : 0}px)`
         }}
         onMouseEnter={() => this.setState({ containerOpacity: 1 })}
         onMouseLeave={() => this.setState({ containerOpacity: 0.8 })}
       >
-        {photos.map((photo, index) =>
-          <div
-            style={{
-              width: 25, height: 25,
-              borderRadius: '50%',
-              backgroundImage: `url(${photo.urls.thumb}`,
-              backgroundSize: 'cover', backgroundPosition: 'center',
-              marginBottom: 7,
-              border: `solid 2px ${idx === index ? 'orange' : 'white'}`,
-              transition: 'all 1s',
-              flexShrink: 0,
-              cursor: 'pointer',
-              opacity: (this.state.hoverItem === index) ? 0.8 : (idx === index ? 1 : 0.6)
-            }}
-            onMouseEnter={() => this.setState({ hoverItem: index })}
-            onMouseLeave={() => this.setState({ hoverItem: null })}
-            onClick={() => this.handleSelect(index)} key={photo.id}
-          />
-        )}
+        <div style={{
+          display: 'flex', flexDirection: 'column',
+          opacity: this.state.containerOpacity || 0.8,
+          transition: 'all .5s',
+        }}>
+          {photos.map((photo, index) =>
+            <div
+              style={{
+                width: 25, height: 25,
+                borderRadius: '50%',
+                backgroundImage: `url(${photo.urls.thumb}`,
+                backgroundSize: 'cover', backgroundPosition: 'center',
+                marginBottom: 7,
+                border: `solid 2px ${idx === index ? 'orange' : 'white'}`,
+                transition: 'all 1s',
+                flexShrink: 0,
+                cursor: 'pointer',
+                opacity: (this.state.hoverItem === index) ? 0.8 : (idx === index ? 1 : 0.6)
+              }}
+              onMouseEnter={() => this.setState({ hoverItem: index })}
+              onMouseLeave={() => this.setState({ hoverItem: null })}
+              onClick={() => this.handleSelect(index)} key={photo.id}
+            />
+          )}
+        </div>
       </div>
     );
   }

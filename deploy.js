99b6427cd803e0e23eb3f2eb9b7dcbc573572cef
Dương Đỗ <dustin.do95@gmail.com>
2017-06-19 00:17:54 +0700

    Fixed wrong deploy

--- deploy.js
+++ deploy.js
@@ -5,12 +5,12 @@ const app = express();
 
 // serve static assets normally
 app.use('/landing', express.static(path.resolve(__dirname, '../public/landing', 'index.html')));
-app.use(express.static(path.resolve(__dirname, '../dist')));
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', (request, response) => {
-  response.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+  response.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port);

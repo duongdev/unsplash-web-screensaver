16e046a4ba9f44b00b2af1019a2598aab961a716
Dương Đỗ <dustin.do95@gmail.com>
2017-06-19 00:15:47 +0700

    Added deploy.js

---
+++ deploy.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8124;
+const app = express();
+
+// serve static assets normally
+app.use('/landing', express.static(path.resolve(__dirname, '../public/landing', 'index.html')));
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+});
+
+app.listen(port);
+console.log(`Server is listening on port ${port}`);
